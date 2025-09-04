d3ff128ae2ac0bc280a5b00fade2e83645f6df61
rhelvey01 <5049404+rhelvey01@users.noreply.github.com>
2025-09-04 18:17:48 +0000

    Added function calling

--- code_template.py
+++ code_template.py
@@ -24,7 +24,7 @@ EXCHANGERATE_API_KEY = os.getenv('EXCHANGERATE_API_KEY')
 
 def get_exchange_rate(base: str, target: str, amount: str) -> Tuple:
     """Return a tuple of (base, target, amount, conversion_result (2 decimal places))"""
-    url = f"https://v6.exchangerate-api.com/v6/{EXCHANGERATE_API_KEY}/pair/{target}/{base}/{amount}"
+    url = f"https://v6.exchangerate-api.com/v6/{EXCHANGERATE_API_KEY}/pair/{base}/{target}/{amount}"
     response = json.loads(requests.get(url).text)
     return(base, target, amount, f'{response["conversion_result"]:.2f}')
 
@@ -33,21 +33,79 @@ print(get_exchange_rate("USD", "EUR", "200"))
 def call_llm(textbox_input) -> Dict:
     """Make a call to the LLM with the textbox_input as the prompt.
        The output from the LLM should be a JSON (dict) with the base, amount and target"""
+    tools = [
+                {
+                    "type": "function",
+                    "function": {
+                        "name": "exchange_rate_function",
+                        "description": "Convert a given amount of money from one currency to another. Each currency will be represented as a 3-letter code",
+                        "parameters": {
+                            "type": "object",
+                            "properties": {
+                                "base": {
+                                    "type": "string",
+                                    "description": "The base or original currency.",
+                                },
+                                "target": {
+                                    "type": "string",
+                                    "description": "The target or converted currency",
+                                },
+                                "amount": {
+                                    "type": "string",
+                                    "description": "The amount of money to convert from the base currency.",
+                                },
+                            },
+                            "required": ["base", "target", "amount"],
+                            "additionalProperties": False,
+                        },
+                    },
+                }
+                ]
+
+
     try:
         response = client.chat.completions.create(
             messages=[
                 {
                     "role": "user",
                     "content": textbox_input,
+                    
                 }
             ],
-            model=model_name
+            model=model_name,
+            tools=tools,
+            tool_choice="auto" 
+            temperature=1.0,
+            top_p=1.0,
+            max_tokens=1000,
+           
         )
+    
        
     except Exception as e:
         print(f"Exception {e} for {Exception}")
     else:
-        return  response.choices[0].message.content
+        return  response#.choices[0].message.content
+
+def run_pipeline(user_input):
+    """Based on textbox_input, determine if you need to use the tools (function calling) for the LLM.
+    Call get_exchange_rate(...) if necessary"""
+
+    response = call_llm(user_input)
+    #st.write(response)
+
+    if response.choices[0].finish_reason == "tool_calls":
+        response_arguments = json.loads(response.choices[0].message.tool_calls[0].function.arguments)
+        base = response_arguments["base"]
+        target = response_arguments["target"]
+        amount = response_arguments["amount"]
+        _, _, _, conversion_result = get_exchange_rate(base, target, amount)
+        st.write(f'{base} {amount} is {target} {conversion_result}')
+    elif response.choices[0].finish_reason == "stop":
+        # Update this
+        st.write(f"(Function calling not used) and {response.choices[0].message.content}")
+    else:
+        st.write("NotImplemented")
 
 st.title("Multilingual Money Changer")
 
@@ -57,6 +115,6 @@ user_input = st.text_input("Enter the amount and currency (e.g., 100 USD):")
 # Create a submit button
 if st.button("Submit"):
     if user_input:
-        st.write(call_llm(user_input))
+        run_pipeline(user_input)
     else:
         st.error("Please enter a valid amount and currency.")
